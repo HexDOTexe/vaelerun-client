c7e78e25c45d570d1cec7f09bbd3ec13d66fcf75
Hex64 <131570879+HexDOTexe@users.noreply.github.com>
2024-02-07 17:57:47 -0600

    Update EntityNode.gd

--- Scenes/Entities/EntityNode.gd
+++ Scenes/Entities/EntityNode.gd
@@ -9,18 +9,3 @@ func _ready():
 	entity_class  = "Machine"
 	entity_level = 10
 	pass
-
-func _physics_process(delta):
-	pass
-
-func _on_hitbox_mouse_entered():
-	UserInterface.mouse_hover(self.name)
-
-func _on_hitbox_mouse_exited():
-	UserInterface.mouse_dehover(self.name)
-
-#func load_resource():
-	#entity_resource = load("res://Resources/Entities/"+str(entity_index_id)+".tres")
-
-func update_entity(new_position):
-	self.position = new_position
